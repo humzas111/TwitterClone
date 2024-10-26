b6cd81072a6277dafd81e3fbc9631dee1db3b6ee
Humza Sohail <humzasohail111@gmail.com>
2024-10-26 21:51:09 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Components/HomePage/HomePage';
+import Authentication from './Components/Authetication/Authentication';
+
+function App() {
+  return (
+    <div className="">
+      <Routes>
+        <Route path='/' element={true?<HomePage />: <Authentication />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
